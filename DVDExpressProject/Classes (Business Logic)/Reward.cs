4bc449998d21ab738e933522f1020f1d99776457
Connor Davenport <csdavenport@crimson.ua.edu>
2016-04-13 17:00:52 -0600

    added some classes to the business layer with some properties; incomplete

---
+++ DVDExpressProject/Classes (Business Logic)/Reward.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVDExpressProject.Classes__Business_Logic_
+{
+    public class Reward
+    {
+        //members and properties
+        //------------------------
+        public int RewardID { get; set; }
+        public int Discount { get; set; }
+        public DateTime Expiration { get; set; }
+        //methods
+        //------------------------
+    }
+}
---
+++ DVDExpressProject/Classes (Business Logic)/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVDExpressProject.Classes__Business_Logic_
+{
+    public class Transaction
+    {
+        //members and properties
+        //------------------------
+        public int TransactionID { get; set; }
+        public int AccountID { get; set; }
+        public int MovieID { get; set; }
+        public int RewardID { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public double Total { get; set; }
+        //methods
+        //------------------------
+    }
+}
