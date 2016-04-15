c38baf5fa8be038ea1ef5c2eed82e23dacc267c4
csdavenport6 <cdavenport626@gmail.com>
2016-04-14 21:35:28 -0500

    started writing data layer for member class

---
+++ DVDExpressProject/DataLayer/MemberDL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+using System.Text;
+
+
+namespace DVDExpressProject.DataLayer
+{
+    class MemberDL
+    {
+        // Members
+        private static string connectionString = ConfigurationManager.ConnectionStrings["MIS220DB"].ConnectionString;
+        // Properties
+        public static string ConnectionString
+        {
+            get { return MemberDL.connectionString; }
+            set { MemberDL.connectionString = value; }
+        }
+    }
+}
