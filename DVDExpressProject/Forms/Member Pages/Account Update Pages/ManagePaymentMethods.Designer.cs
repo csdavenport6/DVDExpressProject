8bb000368b2c11323e2ff87e2fac2327fcdbbfca
jbonatz <jbonatz@crimson.ua.edu>
2016-04-19 10:37:39 -0600

    updated MaanagePaymentMethod and UpdateAddress

--- DVDExpressProject/Forms/Member Pages/Account Update Pages/ManagePaymentMethods.Designer.cs
+++ DVDExpressProject/Forms/Member Pages/Account Update Pages/ManagePaymentMethods.Designer.cs
@@ -28,19 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CardNumberText = new System.Windows.Forms.TextBox();
+            this.CCNText = new System.Windows.Forms.TextBox();
+            this.ExpirationText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fAQToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(285, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Card Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(285, 353);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CCN:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(285, 460);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(244, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Expiration Date:";
+            // 
+            // CardNumberText
+            // 
+            this.CardNumberText.Location = new System.Drawing.Point(540, 246);
+            this.CardNumberText.Name = "CardNumberText";
+            this.CardNumberText.Size = new System.Drawing.Size(336, 31);
+            this.CardNumberText.TabIndex = 3;
+            // 
+            // CCNText
+            // 
+            this.CCNText.Location = new System.Drawing.Point(540, 350);
+            this.CCNText.Name = "CCNText";
+            this.CCNText.Size = new System.Drawing.Size(336, 31);
+            this.CCNText.TabIndex = 4;
+            // 
+            // ExpirationText
+            // 
+            this.ExpirationText.Location = new System.Drawing.Point(540, 457);
+            this.ExpirationText.Name = "ExpirationText";
+            this.ExpirationText.Size = new System.Drawing.Size(336, 31);
+            this.ExpirationText.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(263, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(674, 45);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Update your billing information";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(1236, 42);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(92, 36);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fAQToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(77, 36);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // fAQToolStripMenuItem
+            // 
+            this.fAQToolStripMenuItem.Name = "fAQToolStripMenuItem";
+            this.fAQToolStripMenuItem.Size = new System.Drawing.Size(135, 36);
+            this.fAQToolStripMenuItem.Text = "FAQ";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.SaveButton.Location = new System.Drawing.Point(641, 679);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(295, 92);
+            this.SaveButton.TabIndex = 8;
+            this.SaveButton.Text = "Save Card Information";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(301, 679);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(295, 92);
+            this.CancelButton.TabIndex = 9;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
             // 
             // ManagePaymentMethods
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(927, 777);
+            this.ClientSize = new System.Drawing.Size(1236, 971);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ExpirationText);
+            this.Controls.Add(this.CCNText);
+            this.Controls.Add(this.CardNumberText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ManagePaymentMethods";
             this.Text = "ManagePaymentMethods";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox CardNumberText;
+        private System.Windows.Forms.TextBox CCNText;
+        private System.Windows.Forms.TextBox ExpirationText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fAQToolStripMenuItem;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button CancelButton;
     }
 }
--- DVDExpressProject/Forms/Member Pages/Account Update Pages/UpdateAddress.Designer.cs
+++ DVDExpressProject/Forms/Member Pages/Account Update Pages/UpdateAddress.Designer.cs
@@ -28,19 +28,196 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fAQToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label5 = new System.Windows.Forms.Label();
+            this.StreetText = new System.Windows.Forms.TextBox();
+            this.ZipCodeText = new System.Windows.Forms.TextBox();
+            this.StateText = new System.Windows.Forms.TextBox();
+            this.CityText = new System.Windows.Forms.TextBox();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(308, 251);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Street Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(392, 322);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "City";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(392, 400);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "State";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(357, 475);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 37);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Zip Code";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(1197, 42);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(92, 36);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fAQToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(77, 36);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // fAQToolStripMenuItem
+            // 
+            this.fAQToolStripMenuItem.Name = "fAQToolStripMenuItem";
+            this.fAQToolStripMenuItem.Size = new System.Drawing.Size(135, 36);
+            this.fAQToolStripMenuItem.Text = "FAQ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(391, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(452, 45);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Update your address";
+            // 
+            // StreetText
+            // 
+            this.StreetText.Location = new System.Drawing.Point(564, 251);
+            this.StreetText.Name = "StreetText";
+            this.StreetText.Size = new System.Drawing.Size(374, 31);
+            this.StreetText.TabIndex = 10;
+            // 
+            // ZipCodeText
+            // 
+            this.ZipCodeText.Location = new System.Drawing.Point(564, 475);
+            this.ZipCodeText.Name = "ZipCodeText";
+            this.ZipCodeText.Size = new System.Drawing.Size(374, 31);
+            this.ZipCodeText.TabIndex = 11;
+            // 
+            // StateText
+            // 
+            this.StateText.Location = new System.Drawing.Point(564, 400);
+            this.StateText.Name = "StateText";
+            this.StateText.Size = new System.Drawing.Size(374, 31);
+            this.StateText.TabIndex = 12;
+            // 
+            // CityText
+            // 
+            this.CityText.Location = new System.Drawing.Point(564, 322);
+            this.CityText.Name = "CityText";
+            this.CityText.Size = new System.Drawing.Size(374, 31);
+            this.CityText.TabIndex = 13;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(390, 620);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(229, 91);
+            this.CancelButton.TabIndex = 14;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(679, 620);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(229, 91);
+            this.SaveButton.TabIndex = 15;
+            this.SaveButton.Text = "Save New Address";
+            this.SaveButton.UseVisualStyleBackColor = true;
             // 
             // UpdateAddress
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(898, 730);
+            this.ClientSize = new System.Drawing.Size(1197, 912);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.CityText);
+            this.Controls.Add(this.StateText);
+            this.Controls.Add(this.ZipCodeText);
+            this.Controls.Add(this.StreetText);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "UpdateAddress";
             this.Text = "UpdateAddress";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fAQToolStripMenuItem;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox StreetText;
+        private System.Windows.Forms.TextBox ZipCodeText;
+        private System.Windows.Forms.TextBox StateText;
+        private System.Windows.Forms.TextBox CityText;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Button SaveButton;
     }
 }
